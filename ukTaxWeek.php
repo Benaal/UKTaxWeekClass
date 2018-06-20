98afb568aefc6c85f58a1de8256be95ff6d13aab
Benaal <6371001+Benaal@users.noreply.github.com>
2018-06-20 13:41:40 +0100

    Update ukTaxWeek.php

--- ukTaxWeek.php
+++ ukTaxWeek.php
@@ -19,20 +19,17 @@ Class UKTaxWeeks
     // and optionally $floorDate, which defaults to the UK tax start 
     // date of 06/04
     
-    $tDate = strtotime($wDate);
+    //convert the provided date to a query string/timestamp
+    $qDate = strtotime($wDate);
     
-    $tDay = date("d");
-    $tMonth = date("m");    
-    $tYear = date("Y");
+    //Calculate This Day, This Month and This Year from current datetime
+    $tDay = date("d",$qDate);
+    $tMonth = date("m",$qDate);    
+    $tYear = date("Y",$qDate);
     
-    if($floorDate == "06/04")
-    {
-      $cDay = 6;
-      $cMonth = 4;
-    } else {
-      $cDay = intval(ltrim($floorDate,2));
-      $cMonth = intval(rtrim($floorDate,2));
-    }
+    //Calculate Comparison Day and Comparison Month from floorDate
+    $cDay = intval(ltrim($floorDate,2));
+    $cMonth = intval(rtrim($floorDate,2));
     
     
     if($tMonth > $cMonth)
@@ -40,15 +37,26 @@ Class UKTaxWeeks
       if ($tDay > $cDay)
       {
         //Today is in year 1 - Start of the tax year is in this calendar year
+        $offset = date("W",$floorDate . "/" . $tYear);
         
+        //Week 1 was already $offset weeks into the year. Calculate tax week accordingly
+        $taxWeekNumber = date("W",$qDate) - ($offset - 1);
       }
     } else
     {
       //Today is in year 0 - Start of the tax year was in last calendar year
+      $fullYearWeeks = date("W","31/12/" . $tYear - 1);
+      $floorWeek = date("W",$floorDate . "/" . ($tYear - 1));
       
+      //subtract the base week number from the whole year to account for 53 week years.
+      $offset = $fullYearWeeks - $floorWeek;
+      
+      //calxculate previous years weeks (offset) + this year's week count
+      $taxWeekNumber = $offset + date("W",$qDate);
     }
     
-    return $taxweeknumber
+    return $taxWeekNumber
   }
+  
 }
 ?>
