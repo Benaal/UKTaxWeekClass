4bf90396b70fb2b5fbefff22f053be296b4207f5
Benaal <6371001+Benaal@users.noreply.github.com>
2018-06-20 11:56:25 +0100

    Update ukTaxWeek.php

--- ukTaxWeek.php
+++ ukTaxWeek.php
@@ -1,1 +1,22 @@
+<?php
+/* This file is part of the UK Tax Weeks Class found at 
+ * www.whitehurstmedia.co.uk
+ *
+ * Copyright (C) 2018 Kay Whitehurst - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the GPL 3.0 license, which can be found at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Please consider appropriate references and credits.
+ */
 
+Class UKTaxWeeks
+{
+  
+  function getTaxWeekByDate($Wdate)
+  {
+    $date = strtotime($date);
+    
+  }
+}
+?>
