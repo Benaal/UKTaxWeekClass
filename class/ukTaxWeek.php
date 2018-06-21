b1b987293008cb229a04eb6c957945de01068dc8
Benaal <6371001+Benaal@users.noreply.github.com>
2018-06-21 22:01:34 +0100

    Optimised layout of the code in the class.

--- class/ukTaxWeek.php
+++ class/ukTaxWeek.php
@@ -15,10 +15,16 @@ Class UKTaxWeeks
   
   function getTaxWeekByDate($wDate,$floorDate = "06/04")
   {
-    // Get the Tax week number based upon $wDate - The date you're querying - 
-    // and optionally $floorDate, which defaults to the UK tax start 
-    // date of 06/04
+    /*
+     * Get the Tax week number based upon $wDate - The date you're querying - 
+     * and optionally $floorDate, which defaults to the UK tax start 
+     * date of 06/04
+     */
     
+    //because we use the UK date format dd/mm/yyyy, convert this to US mm/dd/yyyy
+    $bits = explode('/',$wDate);
+    $wDate = $bits[1].'/'.$bits[0].'/'.$bits[2];
+
     //convert the provided date to a query string/timestamp
     $qDate = strtotime($wDate);
     
@@ -26,33 +32,47 @@ Class UKTaxWeeks
     $tDay = date("d",$qDate);
     $tMonth = date("m",$qDate);    
     $tYear = date("Y",$qDate);
-    
+
     //Calculate Comparison Day and Comparison Month from floorDate
-    $cDay = intval(ltrim($floorDate,2));
-    $cMonth = intval(rtrim($floorDate,2));
+    $floorBits = explode('/',$floorDate); 
+    $cDay = $floorBits[0];
+    $cMonth = $floorBits[1];
     
     
-    if($tMonth > $cMonth)
-    {
-      if ($tDay > $cDay)
+    //decide whether the start of the tax year for the given date was this calendar year or last
+    if ($tMonth > $cMonth) {
+      $scenario = 1;
+    } else if ($tMonth == $cMonth) {
+      if ($tDay >= $cDay)
       {
-        //Today is in year 1 - Start of the tax year is in this calendar year
-        $offset = date("W",$floorDate . "/" . $tYear);
-        
-        //Week 1 was already $offset weeks into the year. Calculate tax week accordingly
-        $taxWeekNumber = date("W",$qDate) - ($offset - 1);
+        $scenario = 1;
+      } else {
+        $scenario = 2;
       }
-    } else
+    } else {
+      $scenario = 2;
+    }
+
+    
+
+
+    if($scenario == 1)
     {
+      //Today is in year 1 - Start of the tax year is in this calendar year
+      $offset = date("W",strtotime($floorDate . "/" . $tYear));
+      //Week 1 was already $offset weeks into the year. Calculate tax week accordingly
+      $taxWeekNumber = date("W",$qDate) - ($offset - 2);
+
+    } elseif ($scenario == 2) {
+
       //Today is in year 0 - Start of the tax year was in last calendar year
-      $fullYearWeeks = date("W","31/12/" . $tYear - 1);
-      $floorWeek = date("W",$floorDate . "/" . ($tYear - 1));
-      
+      $fullYearWeeks = date("W",strtotime("31/12/" . ($tYear - 1)));
+      $floorWeek = date("W",strtotime($floorDate . "/" . ($tYear - 1)));
       //subtract the base week number from the whole year to account for 53 week years.
       $offset = $fullYearWeeks - $floorWeek;
-      
-      //calxculate previous years weeks (offset) + this year's week count
+      //calculate previous years weeks (offset) + this year's week count
       $taxWeekNumber = $offset + date("W",$qDate);
+
     }
     
     return $taxWeekNumber;
--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
 //include the class once
-include_once('class/ukTaxWeek.php');
+include_once('./class/ukTaxWeek.php');
 
 //instantiate the class
 $taxWeek = new UKTaxWeeks;
 
 //Get the tax week of the date
 $myDate = "01/06/2018";
-echo $taxWeek->getTaxWeekByDate($myDate);
+echo "The tax week of this year is: " . $taxWeek->getTaxWeekByDate($myDate) . "<br />";
 
 //Get tax week of the date when the financial year starts on another day (1st February, for example - 01/02)
 $financialYearStartDate = "01/02";
-echo $taxWeek->getTaxWeekByDate($myDate,$financialYearStartDate);
+echo "if the tax year started on $financialYearStartDate the financial week would be " . $taxWeek->getTaxWeekByDate($myDate,$financialYearStartDate);
 
 ?>
