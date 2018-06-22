b7afcd44bcabb1158212c4ad24964ed3a7edc623
Benaal <6371001+Benaal@users.noreply.github.com>
2018-06-22 11:26:41 +0100

    New coding

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 
 //include the class once
-include_once('./class/ukTaxWeek.php');
+include_once './class/ukTaxWeek.php';
 
 //instantiate the class
-$taxWeek = new UKTaxWeeks;
+$taxWeek = new UKTaxWeeks();
 
 //Get the tax week of the date
-$myDate = "01/06/2018";
-echo "The tax week of this year is: " . $taxWeek->getTaxWeekByDate($myDate) . "<br />";
+$myDate = '05/04/2018';
+echo "Comparing date $myDate <br />";
+echo 'The tax week of this year is: '.$taxWeek->getTaxWeekByDate($myDate).'<br />';
 
 //Get tax week of the date when the financial year starts on another day (1st February, for example - 01/02)
-$financialYearStartDate = "01/02";
-echo "if the tax year started on $financialYearStartDate the financial week would be " . $taxWeek->getTaxWeekByDate($myDate,$financialYearStartDate);
-
-?>
+$financialYearStartDate = '01/10';
+echo "if the tax year started on $financialYearStartDate the financial week would be ".$taxWeek->getTaxWeekByDate($myDate, $financialYearStartDate);
